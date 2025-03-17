1ae786cd85164a85d16bd90841a4de41927c76a4
Darek Lubomski <darek.lubomski@gmail.com>
2025-03-17 10:40:59 +0100

    Initial commit

---
+++ countries.js
@@ -0,0 +1,1 @@
+var visitedCountries = ["POL", "ESP", "DEU", "PER", "MAR", "TZA", "ITA", "GBR", "SWE", "TUR", "JOR", "CZE", "AUT", "UKR", "ROU", "BGR", "SVN", "HRV", "SRB", "CYP", "BIH", "LTU", "HUN", "FIN", "SVK"];
